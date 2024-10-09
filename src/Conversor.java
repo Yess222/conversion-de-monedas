b29d306bc3844eef225a0c07395ad1d07aaf7dfa
Yess222 <151955473+Yess222@users.noreply.github.com>
2024-10-09 15:31:33 -0500

    Desafio conversor de monedas - Alura Oracle

--- src/Conversor.java
+++ src/Conversor.java
@@ -1,7 +1,3 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-
 public record Conversor(double conversion_rate) {
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,4 @@
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-
 import java.io.IOException;
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
 import java.util.ArrayList;
 import java.util.InputMismatchException;
 import java.util.List;
